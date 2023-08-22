c986e41dc7548c4338ca8dfb4574ca56e510ff31
Zunhokim <zxcrtf245@naver.com>
2023-08-22 17:36:45 +0900

    230812

--- src/chap_01/_03_Variables.java
+++ src/chap_01/_03_Variables.java
@@ -13,6 +13,13 @@ public class _03_Variables {
         name = "Nobody";
 
         System.out.println(name + "의 평균 : " + score + "점, 학점 : " + grade);
+
+        /* 변수의 형식
+        int : 정수형 / long : 큰 수 (끝에 대문자 또는 소문자 L을 붙여야함)
+        double : 실수형 / float : 자릿수가 적은 실수형 (끝에 대문자 또는 소문자 F를 붙여야함)
+        char : 단일 문자 / String : 문자열
+        boolean : true or false
+         */
     }
 }
 
