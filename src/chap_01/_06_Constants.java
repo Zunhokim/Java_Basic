8cafb803882b15f528a36a0f4d0c455b3cccde6b
Zunhokim <zxcrtf245@naver.com>
2023-08-28 16:12:54 +0900

    230828

---
+++ src/chap_01/_06_Constants.java
@@ -0,0 +1,9 @@
+package chap_01;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        final String KR_COUNTRY_CODE = "+82"; // final + 변수명은 상수 선언.
+        final double PI = 3.141592;
+        final String DATE_OF_BIRTH = "2000.01.01";
+    }
+}
