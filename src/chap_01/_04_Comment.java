11e965e3dbd393bf0c0036abaf4860a930e7c207
Zunhokim <zxcrtf245@naver.com>
2023-08-28 14:32:16 +0900

    230828

---
+++ src/chap_01/_04_Comment.java
@@ -0,0 +1,15 @@
+package chap_01;
+
+public class _04_Comment {
+    public static void main(String[] args) {
+        System.out.println("(10분 전) 잠시 후 결혼식 시작 예정이오니 착석 부탁드립니다.");
+        //System.out.println("(5분 전) 잠시 후 결혼식 시작 예정이오니 착석 부탁드립니다.");
+        System.out.println("지금부터 결혼식을 시작하겠습니다.");
+
+        int size = 120;
+        size += 10; // 10 추가
+        System.out.println(size);
+        // 여러 줄 주석 처리를 할 때는 /* */ 로 주석 처리를 진행함.
+        // 단축키 > 한 줄 : Command + / , 여러 줄 : Command + Shift + /
+    }
+}
