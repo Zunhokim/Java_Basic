7e1279b76ac4520cdbdbd3c03e6998b7c3dd8936
Zunhokim <zxcrtf245@naver.com>
2023-09-01 15:52:05 +0900

    230901

--- src/chap_04/_04_SwitchCase.java
+++ src/chap_04/_04_SwitchCase.java
@@ -18,5 +18,20 @@ public class _04_SwitchCase {
             default:
                 System.out.println("대상 아님");
         }
+
+        // 중고 상품의 등급에 따른 가격 책정 (1급 : 최상, 4급 : 최하)
+        int grade = 1;
+        int price = 7000;
+
+        switch (grade) {
+            case 1:
+                price += 1000;
+            case 2:
+                price += 1000;
+            case 3:
+                price += 1000;
+                break;
+        }
+        System.out.println(price);
     }
 }
